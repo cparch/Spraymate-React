12e4d4540166550d5bb11f063c285656b3b89c60
Chad Archila <chadparchila@gmail.com>
2018-12-26 15:42:35 -0800

    add browser router

--- src/index.js
+++ src/index.js
@@ -6,8 +6,17 @@ import registerServiceWorker from './registerServiceWorker';
 import {createStore} from 'redux'
 import reducer from './store/reducer';
 import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
 
 const store = createStore(reducer);
 
-ReactDOM.render(<Provider store={store}><App /> </Provider>, document.getElementById('root'));
+const app = (
+	<Provider store ={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
