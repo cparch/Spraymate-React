a13bb43795cc2e83d3ee6d0528adf53f89c3a8d7
Chad Archila <chadparchila@gmail.com>
2019-01-02 21:48:39 -0800

    added css to testimonial and added divs to testimonial.js

--- src/component/testimonial/testimonial.js
+++ src/component/testimonial/testimonial.js
@@ -3,7 +3,14 @@ import classes from './testimonial.css';
 
 const Testimonial = () => {
     return (
-        <div className={classes.testimonial}> test from Testimonial</div>
+        <div className={classes.mainContainer}>
+            <div className={classes.individualContainer}>'SprayMate's results were so far superior and all done with minimum fuss and bother'.</div>
+            <div className={classes.readMore}> - Read More</div>
+            <div className={classes.individualContainer}>'No matter how carefully I look, the piece seems as good as when I brought it home'.</div>
+            <div className={classes.readMore}> - Read More</div>
+            <div className={classes.individualContainer}>'I have been using Spraymate for ten years. As a professional painting contractor I appreciate great quality and timely service'.</div>
+            <div className={classes.readMore}> - Read More</div>
+        </div>
     );
 };
 
