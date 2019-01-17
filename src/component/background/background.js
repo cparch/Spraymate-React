e065f23b193d2af9393bff9574355c680625c7fa
Chad Archila <chadparchila@gmail.com>
2019-01-16 18:30:48 -0800

    moved background display to it's own component

---
+++ src/component/background/background.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react";
+import * as actionTypes from '../../store/actions';
+import {connect} from 'react-redux';
+import classes from './background.css';
+
+
+
+class Background extends Component {
+
+      updateImg = () => {
+    //   document.body.style.background = `url(${this.props.imageUrl[this.props.count]})`; 
+      document.body.style.background = `url(${this.props.imageUrl[3]})`; 
+      document.body.style.backgroundRepeat = "no repeat";
+      document.body.style.backgroundPosition = 'center';
+      document.body.style.backgroundSize = 'cover';
+      document.body.style.backgroundAttachment = 'fixed'
+    };
+
+    render() {
+        return (
+
+        <div>
+           <div className = {classes.white}> {this.props.imageUrl[this.props.count]} </div>
+           <div> {this.updateImg()} </div>
+        </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+       count: state.count,
+       imageUrl: state.images
+    };
+ };
+ 
+ const mapDispatchToProps = dispatch => {
+   return {
+    // read_more: (e) => dispatch({type: actionTypes.READ_ME, shortText: e}),
+    // leftBtnClick: () => dispatch({type: actionTypes.REMOVE_FROM_COUNT}),
+   }  
+ }; 
+ 
+export default connect(mapStateToProps, mapDispatchToProps)(Background);
--- src/component/gallery/gallery.js
+++ src/component/gallery/gallery.js
@@ -10,52 +10,34 @@ library.add(faArrowCircleLeft, faArrowCircleRight)
 
 class LeftScrollBtn extends Component {
 
-    updateImg = () => {
-      document.body.style.background = `url(${this.props.images[this.props.count]})`; 
-      document.body.style.backgroundRepeat = "no repeat";
-      document.body.style.backgroundPosition = 'center';
-      document.body.style.backgroundSize = 'cover';
-      document.body.style.backgroundAttachment = 'fixed'
-    };
+    // updateImg = () => {
+    //   document.body.style.background = `url(${this.props.images[this.props.count]})`; 
+    //   document.body.style.backgroundRepeat = "no repeat";
+    //   document.body.style.backgroundPosition = 'center';
+    //   document.body.style.backgroundSize = 'cover';
+    //   document.body.style.backgroundAttachment = 'fixed'
+    // };
 
-    // f1 runs then, 1 ml sec goes by and the background image will update
-   ClickHandler = (f1) => {
-      f1;
-      setTimeout(this.updateImg, 1)
-    };
+  //  ClickHandler = (f1) => {
+  //     f1;
+  //     setTimeout(this.updateImg, 1)
+  //   };
+
+  ClickHandler = (f1) => {
+    f1;
+  };
  
     
     render() {
-
-      // console.log('count ' + this.props.count + "  from render");
-      // console.log("img length " + this.props.images.length)
       
       return (
         <div className={classes.body}>
 
-            {/* v works, but calls the last count, not the current count. I want it to change the count, then update the image. */}
-
-            {/* <div className = {classes.leftArrow} 
-              onClick={() => { this.props.leftBtnClick(); this.updateImg();}}> 
-              <FontAwesomeIcon icon="arrow-circle-left" /> 
-            </div> */}
-
-
-              {/* v call back function v */}
               <div className = {classes.leftArrow} 
                   onClick={() => {this.ClickHandler(this.props.leftBtnClick());}}> 
                   <FontAwesomeIcon icon="arrow-circle-left" /> 
               </div>
 
-
-              {/* v works, but calls the last count, not the current count. I want it to change the count, then update the image. */}
-              
-              {/* <div className={classes.rightArrow} 
-                onClick={() => { this.props.rightBtnClick(); this.updateImg();}}> 
-                <FontAwesomeIcon icon="arrow-circle-right" /> 
-              </div> */}
-
-              {/* v call back function v */}
               <div className = {classes.rightArrow} 
                   onClick={() => {this.ClickHandler(this.props.rightBtnClick());}}> 
                   <FontAwesomeIcon icon="arrow-circle-right" /> 
@@ -75,12 +57,8 @@ class LeftScrollBtn extends Component {
  const mapDispatchToProps = dispatch => {
    return {
      rightBtnClick: () => dispatch({type: actionTypes.ADD_TO_COUNT}),
-     leftBtnClick: () => dispatch({type: actionTypes.REMOVE_FROM_COUNT}),
-    //  onRemovedPerson: (id) => dispatch({type: actionTypes.REMOVE_PERSON, personId: id})
-   
+     leftBtnClick: () => dispatch({type: actionTypes.REMOVE_FROM_COUNT}),   
    }  
  }; 
  
- 
-
   export default connect(mapStateToProps, mapDispatchToProps)(LeftScrollBtn);
