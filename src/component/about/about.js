a8d7f2800846a3603f1430a941cb080c60f46cad
Chad Archila <chadparchila@gmail.com>
2019-01-17 17:15:54 -0800

    added text to about section

--- src/component/about/about.js
+++ src/component/about/about.js
@@ -4,7 +4,14 @@ import classes from './about.css';
 const About = () => {
     return (
         <div className = {classes.about}> 
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+            Lorem ipsum dolor sit amet, metus pellentesque tellus, ac urna eleifend ligula, justo viverra rhoncus, mauris mauris mauris morbi nunc. Lectus nec mi massa eu lacinia, non nam pellentesque eu sit, diam metus mi quis, sit non sed venenatis, sapien ea blandit pharetra. Pharetra lobortis sollicitudin nibh eros, euismod nam porttitor, turpis erat arcu ante vestibulum. Vulputate sapien tristique nisl amet conubia, ante faucibus, dolor phasellus eu, vel sit mauris volutpat vitae. Auctor eros risus. Justo ornare, et a diam et, id id, consequat arcu mus nisl turpis. Dolor dictum sed ut. Eu porta luctus autem, placerat metus nullam inceptos erat pellentesque sollicitudin, ut gravida sed at phasellus duis tristique, non morbi volutpat mi mauris sed dignissim. Pellentesque ante ultrices quis hac aliquet, vel est morbi, sed amet pede erat mollis est.
+
+
+Penatibus mollis, ac duis mattis dapibus ut magna bibendum, nullam malesuada. Eleifend et etiam nullam. Urna ultricies ac diam cupidatat id, orci vestibulum, in libero maecenas mi, mus donec orci est, urna tortor. Vivamus rutrum quis, vel vel, vitae in vestibulum lacinia libero varius vestibulum, dictum mollis ac nulla in, mauris feugiat. Justo ipsum, cras quam scelerisque, condimentum non curabitur et sed ac, nulla tortor quisquam.
+
+Risus scelerisque, volutpat eros quis mi tortor neque velit. Hendrerit imperdiet quam mauris, sed dapibus, amet nam morbi quam ac, morbi nibh urna morbi sed a. Massa risus, suspendisse etiam, et vivamus et magna ornare nec, eget ut risus donec. Aliquam debitis pellentesque at semper. Nec lacinia alias. Commodo dictum, nam dui diam mattis fringilla. Auctor et aenean nullam, elit vulputate phasellus vestibulum curabitur a integer. Rutrum morbi pellentesque, mauris sodales nulla nam libero, eleifend pharetra, tortor quis vel. Elit odio accumsan erat nunc ut praesent. Curabitur aliquam proin gravida eu torquent, nec proin. Leo volutpat sed aliquam posuere cras purus, et sit, eu eget quis. Mi ut nulla massa, sed varius amet fermentum, pulvinar ligula ut in et hendrerit nunc, auctor posuere pellentesque accumsan quam justo.
+
+
 
         </div>
     )
