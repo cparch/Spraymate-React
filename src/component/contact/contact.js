7ec3148ff7a31cb79b416dee1106e84402fa9051
Chad Archila <chadparchila@gmail.com>
2019-01-24 15:53:23 -0800

    added div

--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -4,7 +4,8 @@ import classes from './contact.css';
 const Contact = () => {
     return (
         <div className={classes.contact}>
-            test from contact. blah blah
+            <div> test from contact. blah blah </div>
+            <div> Second paragraph added on 1/24/19 </div>
         </div>
     );
 };
