dca37fef6957e18512363579e253829b5c788b64
Chad Archila <chadparchila@gmail.com>
2019-01-24 16:07:50 -0800

    deleted test from test branch

--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -4,8 +4,7 @@ import classes from './contact.css';
 const Contact = () => {
     return (
         <div className={classes.contact}>
-            <div> test from contact. blah blah </div>
-            <div> Second paragraph added on 1/24/19 </div>
+            <div> test from contact</div>
         </div>
     );
 };
