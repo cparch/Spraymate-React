f01307933b458091310a05c79c393ca9bc559e0f
Chad Archila <chadparchila@gmail.com>
2019-01-24 16:25:39 -0800

    deleted backgorund.css

--- src/component/about/about.js
+++ src/component/about/about.js
@@ -5,7 +5,6 @@ const About = () => {
     return (
         <div className = {classes.about}> 
             We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
-
         </div>
     )
 }
