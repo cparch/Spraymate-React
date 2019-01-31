3a5ed30146fd74580446def754bc0a7fa858ab28
Chad Archila <chadparchila@gmail.com>
2019-01-30 18:38:20 -0800

    added navBar.js and navBar.css

--- src/navBar.js
+++ src/navBar.js
@@ -1,32 +1,17 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import classes from './navBar.css';
+// import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+  return (
 
+    <header className={classes.navBar}> 
+      <div>MENU</div>
+      <nav> 
+        ...
+      </nav>
+    </header>
 
-    return <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">Spraymate</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-  
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-          <Link className="nav-link" to="/Gallery" >Gallery <span className="sr-only">(current)</span></Link>
-        </li>
-        <li className="nav-item active">
-          <Link className="nav-link" to="/About">About <span className="sr-only">(current)</span></Link>
-        </li>
-        <li className="nav-item active">
-          <Link className="nav-link" to="/Testimonial">Testimonial <span className="sr-only">(current)</span></Link>
-        </li>
-        <li className="nav-item active">
-          <Link className="nav-link" to="/Contact">Contact <span className="sr-only">(current)</span></Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
-};
+  )};
 
 export default NavBar;
