12af9cf0eec25ef286191ae300d4ca1e42dce007
Chad Archila <chadparchila@gmail.com>
2019-02-04 17:08:39 -0800

    added JSX for backdrop.js

--- src/component/Navigation/sideDrawer/BackDrop/BackDrop.js
+++ src/component/Navigation/sideDrawer/BackDrop/BackDrop.js
@@ -1,8 +1,103 @@
-import React from 'react';
+import React, {Component} from "react";
 import classes from './BackDrop.css'
+import * as actionTypes from '../../../../store/actions';
+import {connect} from 'react-redux';
 
-const BackDrop = () => (
-    <div className = {classes.BackDrop}></div>
-);
+class BackDrop extends Component {
 
-export default BackDrop;
+    render() {
+
+                // if showside drawer === true then I need this component to display
+
+        let style = {
+            display: "none"
+        }
+
+        if(this.props.sidedrawerShowing === true) {
+            style.display = "block"
+        }
+
+        return (
+        <div className = {classes.BackDrop} 
+            style = {style}
+            onClick = {() => {this.props.Toggle()}}></div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+    //    count: state.count,
+       sidedrawerShowing: state.showSideDrawer,
+    };
+ };
+ 
+ const mapDispatchToProps = dispatch => {
+   return {
+    // read_more: (i) => dispatch({type: actionTypes.READ_ME, indexSport: i}),
+    // read_less: (i) => dispatch({type: actionTypes.READ_LESS, idx: i})
+    Toggle: () => dispatch({type: actionTypes.TOOGLE_SIDEDRAWER})
+   }  
+ }; 
+ 
+export default connect(mapStateToProps, mapDispatchToProps)(BackDrop);
+
+
+
+
+
+
+
+// import React, {Component} from "react";
+// import classes from './testimonialList.css';
+// import * as actionTypes from '../../store/actions';
+// import {connect} from 'react-redux';
+
+// class TestimonialList extends Component {
+
+//     render() {
+
+//         const testimonialLists = this.props.showTestimonial.map((e, i) => {
+
+//             let moreLessButton = "- read more"
+
+//             if (this.props.showTestimonial[i].showSample === false){
+//                 moreLessButton = null
+//             }
+            
+//             return (
+//                 <div>
+//                     <div className = {classes.individualContainer}> {e.text} </div> 
+
+//                     <div className = {classes.readMore}
+//                         onClick = {() => {this.props.read_more(i)}}>{moreLessButton}  
+//                     </div>
+
+//                 </div>)
+//                 });
+//         return (
+
+//         <div className={classes.mainContainer}>
+//             {testimonialLists}
+//         </div>
+//         )
+//     }
+// }
+
+// const mapStateToProps = state => {
+//     return {
+//        count: state.count,
+//        readMe: state.readMe,
+//        showTestimonial: state.showTestimonial,
+//        fullTestimonial: state.fullParagraph
+//     };
+//  };
+ 
+//  const mapDispatchToProps = dispatch => {
+//    return {
+//     read_more: (i) => dispatch({type: actionTypes.READ_ME, indexSport: i}),
+//     read_less: (i) => dispatch({type: actionTypes.READ_LESS, idx: i})
+//    }  
+//  }; 
+ 
+// export default connect(mapStateToProps, mapDispatchToProps)(TestimonialList);
