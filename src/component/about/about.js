cfb3d5b949bce6b3f939e06ca1c0368b8557da30
Chad Archila <chadparchila@gmail.com>
2019-02-09 11:34:40 -0800

    cleaned up code

--- src/component/about/about.js
+++ src/component/about/about.js
@@ -1,38 +1,34 @@
 import React from 'react';
 import classes from './about.css';
 
-const About = () => {
-    return (
-        <div className = {classes.about}> 
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+const About = () => (
+    <div className={classes.about}> 
+        We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+        We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
 
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+        We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
 
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+        We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
 
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+        We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
 
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+        We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+        We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
 
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+        We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
 
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+        We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
 
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+        We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
 
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+        We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
 
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+        We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
 
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+        We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
 
-            We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
-
-
-        </div>
-    )
-}
+        We have been in business since 1986. We do customer finishing of shutter, kitchens, bedrooms, and furniture
+    </div>
+)
 
 export default About;
