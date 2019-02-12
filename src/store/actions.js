e19a8f78d5501f8f6db11dbf02eb425ca4b38b14
Chad Archila <chadparchila@gmail.com>
2019-02-11 18:17:46 -0800

    added a ; after "READ_LESS"

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,7 +1,7 @@
 export const ADD_TO_COUNT = 'ADD_TO_COUNT';
 export const REMOVE_FROM_COUNT = 'REMOVE_FROM_COUNT';
 export const READ_ME = 'READ_ME';
-export const READ_LESS = 'READ_LESS'
+export const READ_LESS = 'READ_LESS';
 export const TOOGLE_SIDEDRAWER = 'TOOGLE_SIDEDRAWER';
 
 
