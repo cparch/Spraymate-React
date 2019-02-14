a782bc95c03df431b683ff189f8253428cc27cfb
Chad Archila <chadparchila@gmail.com>
2019-02-13 21:10:13 -0800

    added constants, added if statmet with jsx

--- src/component/testimonialList/testimonialList.js
+++ src/component/testimonialList/testimonialList.js
@@ -8,23 +8,40 @@ class TestimonialList extends Component {
     render() {
 
         const testimonialLists = this.props.showTestimonial.map((e, i) => {
+            const fullParagraph = ["I did a major upgrade on my condo. Rather than replace my old, solid wood kitchen cabinets, I contracted with SprayMate to paint the old cabinet boxes and install freshly painted, MDF constructed top to bottom and side to side drawers and doors using a semigloss paint with interior hinges replacing exterior hinges. The finished project turned out exactly the way I'd hoped. What had been funky, 1960's era cabinets now looked like they'd been replaced with the brand new, contemporary, clean white European cabinets. While a bit more expensive than most of the painters I talked with, SprayMate's results were so far superior and all done with minimum fuss and bother, efficiently and great responsiveness to my questions and preferences. I can't recommend SprayMate more highly.",
+            "When a plumber accidentally splashed wastewater with drain cleaner on a painted TV console, I was convinced it was destroyed. And then came Rick and Stacey to the rescue! They looked at the pictures, said 'no problem', gave me a fast and fair quote and answered all of my questions with grace and speed. The only pain was slogging through traffic to get the console from my Daly City home to their shop in Newark. No matter how carefully I look, the piece seems as good as when I brought it home. So glad to have found them and happy to give them my highest recommendation!",
+            "I have been using Spraymate for ten years. As a professional painting contractor I appreciate great quality and timely service. Rick and Stacy have come through for me time and time again."]
 
-            let moreLessButton = "- read more"
+            const sampleTestimony = ["SprayMate's results were so far superior and all done with minimum fuss and bother",
+            "No matter how carefully I look, the piece seems as good as when I brought it home",
+            "I have been using Spraymate for ten years. As a professional painting contractor I appreciate great quality and timely service"]
 
-            // if (this.props.showTestimonial[i].showSample === false){
-                if (!this.props.showTestimonial[i].showSample){
-                moreLessButton = null
+
+            
+
+            if (!this.props.showTestimonial[i].showSample){
+                return (
+                   <div>
+                        <div className = {classes.individualContainer}> {fullParagraph[i]} </div> 
+
+                        <div className = {classes.readMore}
+                        onClick = {() => {this.props.read_less(i)}}> -read less </div>
+                        {/* onClick = {() => {console.log(i)}}> -read less </div> */}
+                   </div>
+                )
+            } else {
+                return(
+                    <div>
+                        <div className = {classes.individualContainer}> {sampleTestimony[i]} </div> 
+
+                        <div className = {classes.readMore}
+                            onClick = {() => {this.props.read_more(i)}}> -read more 
+                        </div>
+
+                    </div>
+                )
             }
             
-            return (
-                <div>
-                    <div className = {classes.individualContainer}> {e.text} </div> 
-
-                    <div className = {classes.readMore}
-                        onClick = {() => {this.props.read_more(i)}}>{moreLessButton}  
-                    </div>
-
-                </div>)
                 });
         return (
 
@@ -37,10 +54,7 @@ class TestimonialList extends Component {
 
 const mapStateToProps = state => {
     return {
-       count: state.count,
-       readMe: state.readMe,
        showTestimonial: state.showTestimonial,
-       fullTestimonial: state.fullParagraph
     };
  };
  
