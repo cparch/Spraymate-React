2994f043b2fd6c1900b20849118d767f7543429c
Chad Archila <chadparchila@gmail.com>
2019-02-20 21:32:08 -0800

    hard coded component to add mailto feature

--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import classes from './contact.css';
-import ContactIndividual from './contactIndividual/contactIndividual';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPhone, faEnvelope)
 
 const Contact = (props) => {
     return (
-        <div className={classes.mainContainer}>
-            <ContactIndividual
-            header = 'Call Us'
-            symbol = "phone"
-            contactInfo= '510-796-7922' />
-
-            <ContactIndividual
-            header = 'Email Us'
-            symbol = 'envelope'
-            contactInfo= 'spraymate@comcast.net' />
-        </div>
+            <div className={classes.mainContainer}>
+                <div className = {classes.container}>
+                    <header>Call Us</header>
+                    <div className={classes.symbol}> 
+                        <FontAwesomeIcon icon="phone"/> 
+                    </div>
+                     <div className={classes.contactInfo}> 510-796-7922 </div>
+                </div>
+                <div className = {classes.container}>
+                    <header>Email Us</header>
+                    <div className={classes.symbol}>  
+                        <FontAwesomeIcon icon="envelope"/> 
+                    </div>
+                    <div className={classes.contactInfo}> 
+                        <a href="mailto:spraymate@comcast.net">spraymate@comcast.net</a>
+                    </div>
+                </div>
+            </div>
     )
 };
 
-export default Contact;
+export default Contact
