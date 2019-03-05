49e418889b44cfef2754b0e6101f9d08306a5784
Chad Archila <chadparchila@gmail.com>
2019-03-04 18:59:56 -0800

    added componentDidMount and ComponentWillUnmount

--- src/component/gallery/gallery.js
+++ src/component/gallery/gallery.js
@@ -10,18 +10,36 @@ import photo4 from '../../images/4.jpg'
 import photo5 from '../../images/5.jpeg'
 import photo6 from '../../images/6.jpg'
 
-
 class DemoCarousel extends Component {
+
+  componentDidMount(){
+    document.body.style.background = `url(https://static-cdn.jtvnw.net/jtv_user_pictures/e91a3dcf-c15a-441a-b369-996922364cdc-profile_image-300x300.png)`; 
+}
+
+componentWillUnmount(){
+  document.body.style.background = `url(https://holdennewhomes.files.wordpress.com/2015/02/6838-ranger-rd-frisco-tx-mls-7.jpg)`; 
+  document.body.style.backgroundRepeat = "no repeat";
+  document.body.style.backgroundPosition = 'center';
+  document.body.style.backgroundSize = 'cover';
+  document.body.style.backgroundAttachment = 'fixed'
+}
+
+
+
+
+
   render() {
     return (
-      <Carousel 
-      showThumbs={false}
-      useKeyboardArrows={true}>
-          <img class="gallery--photo" src= {photo1} />
-          <img class="gallery--photo" src= {photo3} />
-          <img class="gallery--photo" src= {photo4} />
-          <img class="gallery--photo" src= {photo6} />
-      </Carousel>
+      <div class="gallery">
+        <Carousel 
+          showThumbs={false}
+          useKeyboardArrows={true}>
+              <img class="gallery--photo" src= {photo1} />
+              <img class="gallery--photo" src= {photo3} />
+              <img class="gallery--photo" src= {photo4} />
+              {/* <img class="gallery--photo" src= {photo6} /> */}
+        </Carousel>
+      </div>
     );
   }
 };
