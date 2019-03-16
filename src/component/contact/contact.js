4459569095d6ed20f5f65c0a06c2f3b1ff5b4401
Chad Archila <chadparchila@gmail.com>
2019-03-16 08:30:31 -0700

    updating css to module.css

--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from './contact.css';
+import classes from './contact.module.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'
@@ -8,20 +8,20 @@ library.add(faPhone, faEnvelope)
 
 const Contact = (props) => {
     return (
-            <div className={classes['contacts--mainContainer']}>
-                <div className = {classes['contacts--container']}>
+            <div className={classes.mainContainer}>
+                <div className = {classes.container}>
                     <header>Call Us</header>
-                    <div className={classes['contacts--symbol']}> 
+                    <div className={classes.symbol}> 
                         <FontAwesomeIcon icon="phone"/> 
                     </div>
-                     <div className={classes['contacts--contactInfo']}> 510-796-7922 </div>
+                     <div className={classes.contactInfo}> 510-796-7922 </div>
                 </div>
-                <div className = {classes['contacts--container']}>
+                <div className = {classes.container}>
                     <header>Email Us</header>
-                    <div className={classes['contacts--symbol']}>  
+                    <div className={classes.symbol}>  
                         <FontAwesomeIcon icon="envelope"/> 
                     </div>
-                    <div className={classes['contacts--contactInfo']}> 
+                    <div className={classes.contactInfo}> 
                         <a href="mailto:spraymate@comcast.net">spraymate@comcast.net</a>
                     </div>
                 </div>
