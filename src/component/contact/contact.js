8f9004f883b2fec172c1e7b62e6da29530ac400d
Chad Archila <chadparchila@gmail.com>
2019-03-23 15:21:37 -0700

    added contact form

--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -8,24 +8,45 @@ library.add(faPhone, faEnvelope)
 
 const Contact = (props) => {
     return (
-            <div className={classes.mainContainer}>
-                <div className = {classes.container}>
-                    <header>Call Us</header>
-                    <div className={classes.symbol}> 
-                        <FontAwesomeIcon icon="phone"/> 
+            <div>
+                <form className = {classes.contactFormMainContainer} method="POST" action="https://formspree.io/chadparchila@gmail.com">
+                    <p className={classes.contactFormHeader}> Contact Us </p>
+                    <p> Email Address </p>
+                    <input 
+                        className={classes.contactFormEmail} 
+                        type="email" name="email" 
+                        placeholder="Your email"/>
+                    <p> Message </p>
+                    <textarea 
+                        className={classes.contactFormTextArea}
+                        name="message" 
+                        placeholder="Type your message for Spraymate here!"/>
+                   
+                    <button 
+                        className = {classes.contactFormSubmitBtn}
+                        type="submit"> Submit Message
+                    </button>
+                </form>
+                <div className={classes.mainContainer}>
+                    <div className = {classes.container}>
+                        <header>Call Us</header>
+                        <div className={classes.symbol}> 
+                            <FontAwesomeIcon icon="phone"/> 
+                        </div>
+                        <div className={classes.contactInfo}> 510-796-7922 </div>
                     </div>
-                     <div className={classes.contactInfo}> 510-796-7922 </div>
-                </div>
-                <div className = {classes.container}>
-                    <header>Email Us</header>
-                    <div className={classes.symbol}>  
-                        <FontAwesomeIcon icon="envelope"/> 
-                    </div>
-                    <div className={classes.contactInfo}> 
-                        <a href="mailto:spraymate@comcast.net">spraymate@comcast.net</a>
+                    <div className = {classes.container}>
+                        <header>Email Us</header>
+                        <div className={classes.symbol}>  
+                            <FontAwesomeIcon icon="envelope"/> 
+                        </div>
+                        <div className={classes.contactInfo}> 
+                            <a href="mailto:spraymate@comcast.net">spraymate@comcast.net</a>
+                        </div>
                     </div>
                 </div>
             </div>
+           
     )
 };
 
