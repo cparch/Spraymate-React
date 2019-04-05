1bf3f9029ccba3b8a13ebf69ed6bfd8f46a2b156
Chad Archila <chadparchila@gmail.com>
2019-04-05 11:05:41 -0700

    updateing redux code: renamed file to actionType

---
+++ src/store/actionType.js
@@ -0,0 +1,8 @@
+export const ADD_TO_COUNT = 'ADD_TO_COUNT';
+export const REMOVE_FROM_COUNT = 'REMOVE_FROM_COUNT';
+export const READ_ME = 'READ_ME';
+export const READ_LESS = 'READ_LESS';
+export const TOOGLE_SIDEDRAWER = 'TOOGLE_SIDEDRAWER';
+
+
+
